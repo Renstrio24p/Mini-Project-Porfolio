db63364949ff6037bade60c2bc0f543798b97200
Waren Gador <123795328+Renstrio24p@users.noreply.github.com>
2023-03-25 01:22:16 +0800

    add analytics section and js

--- js/myscript.js
+++ js/myscript.js
@@ -14,3 +14,55 @@ let typed = new Typed('.auto-input', {
     backDelay:  2000,
     loop: true,
 })
+
+/* Donut Chart */
+
+let number = document.getElementById("number");
+let counter = 0;
+
+setInterval(() => {
+
+    if(counter == 55) {
+        clearInterval();
+    } else {
+        counter += 1;
+        number.innerHTML = counter + " %";
+    }
+},28);
+
+let number2 = document.getElementById("number2");
+let counter2 = 0;
+
+setInterval(() => {
+
+    if(counter2 == 45) {
+        clearInterval();
+    } else {
+        counter2 += 1;
+        number2.innerHTML = counter2 + " %";
+    }
+},26);
+let number3 = document.getElementById("number3");
+let counter3 = 0;
+
+setInterval(() => {
+
+    if(counter3 == 45) {
+        clearInterval();
+    } else {
+        counter3 += 1;
+        number3.innerHTML = counter3 + " %";
+    }
+},26);
+let number4 = document.getElementById("number4");
+let counter4 = 0;
+
+setInterval(() => {
+
+    if(counter4 == 67) {
+        clearInterval();
+    } else {
+        counter4 += 1;
+        number4.innerHTML = counter4 + " %";
+    }
+},15);
